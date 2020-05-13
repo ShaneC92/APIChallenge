c114531f70879012784b166abe99fc723ffb588d
Shane Cox <shanecox@MacBook-Air.local>
2020-05-13 19:53:14 -0400

    second push

--- api.js
+++ api.js
@@ -9,14 +9,16 @@ submitBtn.addEventListener("click", fetchResults);
 function fetchResults(e) {
     e.preventDefault();
     console.log(e);
-    url = baseURL;
+    url = `${baseURL}?&limit=1`;
     console.log(url);
 
     fetch(url)
     .then(function(results) {
+        console.log(results);
         return results.json();
     })
     .then(function(json) {
+        console.log(json);
         displayResults(json);
     })
 
@@ -30,19 +32,40 @@ function displayResults(json) {
     console.log("DisplayResults", json);
     let people = json;
 
+
+
     if(people.length === 0) {
         console.log("No Results");
     } else {
         for(let i = 0; i < people.length; i++) {
             console.log(i);
+            
 
-            let people = document.createElement("article");
+            let article = document.createElement("article");
+            let name = document.createElement("h1")
             let heading = document.createElement("h2");
+            let gender = document.createElement("p");
+            let ageInfo = document.createElement("p")
+            let eyecolorInfo = document.createElement("p");
+            let current = people[i];
+            name.textContent = "Name: " + current.name;
+            gender.innerText = "Gender: " + current.gender;
+            ageInfo.innerText = "Age: " + current.age;
+            eyecolorInfo.innerText = "Eye Color: " + current.eye_color;
+      
+            article.appendChild(heading);
+            heading.appendChild(name);
+            heading.appendChild(gender);
+            heading.appendChild(ageInfo);
+            heading.appendChild(eyecolorInfo);
+            section.appendChild(article);
 
 
+            // let people = document.createElement("article");
+            // let heading = document.createElement("h2");
 
-            people.appendChild(heading);
-            section.appendChild(people);
+            // // people.appendChild(heading);
+            // section.appendChild(people);
         }
     }
 
